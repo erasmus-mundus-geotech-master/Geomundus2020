ba7b3aa4b1d8beb85102cc802c044e34faef17e3
duckravel <al396421@uji.es>
2020-05-14 11:08:52 +0800

    clear some js codes, update map info

--- js/main.js
+++ js/main.js
@@ -33,24 +33,20 @@ var imageAirport = {
     scaledSize: new google.maps.Size(32, 32)
 };
 
-var ujiTooltip = 'Conference Venue: “Institute for Geoinformatics (IFGI)”, University of Münster'
+var VenueTooltip = 'Conference Venue: “Institute for Geoinformatics (IFGI)”, University of Münster'
 
 var WeezeAirport;
 var MünsterAirport;
 var DusselAirport;
 var BonnAirport;
 var hotelSchlosspark;
-var ValenciaTrain;
-var BarcelonaTrain;
-var MadridTrain;
-
 var university;
 var Dinner;
 var MeetUp;
-
+//infowindo
 var infoVenue,infoMünsterAirport,infoWeezeAirport,infoDusselAirport,infoBonnAirport,
 infoDinnerLoc,infoMeetUp,infoSchlosspark;
-
+//venue info window content
 var contentWwu = '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
                     '<h2 style="color:#4686A0">Institute for Geoinformatic</h2>' +
                     '<p align="center" style="color:#4a87d3">' +
@@ -80,8 +76,6 @@ function clearInforWindows() {
     if (infoMeetUp) infoMeetUp.close();
     if (infoSchlosspark) infoSchlosspark.close();
 
-
-
 }
 
 function setMapVisibility(itemClicked) {
@@ -91,20 +85,17 @@ function setMapVisibility(itemClicked) {
     clearObjectFromMap(MünsterAirport);
     clearObjectFromMap(DusselAirport);
 
-    //clearObjectFromMap(university);
     clearObjectFromMap(Dinner);
     clearObjectFromMap(MeetUp);
 
     clearObjectFromMap(hotelSchlosspark);
-    clearObjectFromMap(ValenciaTrain);
-    clearObjectFromMap(BarcelonaTrain);
-    clearObjectFromMap(MadridTrain);
 
     switch (itemClicked) {
         case "flight":
             {
-                //Airports
-                var contentWeezeAirport = '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
+                //Airports markers/pop up create
+                infoWeezeAirport = new google.maps.InfoWindow({
+                    content: '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
                     '<h2 style="color:#4686A0">Weeze International Airport</h2>' +
                     '<p align="center" style="color:#4a87d3">' +
                     '<b style="color:#4a87d3">Telephone:</b> +49 2837 666111</br>' +
@@ -112,9 +103,6 @@ function setMapVisibility(itemClicked) {
                     'airport-weeze.de</a></p>' +
                     '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://goo.gl/maps/52dZZwgqrrQgMhQg6">Navigate</a></h3>' +
                     '</div>'
-
-                infoWeezeAirport = new google.maps.InfoWindow({
-                    content: contentWeezeAirport
                 });
 
                 WeezeAirport = new google.maps.Marker({
@@ -177,9 +165,7 @@ function setMapVisibility(itemClicked) {
                     icon: imageAirport,
                     // animation: google.maps.Animation.BOUNCE,
                 });
-
-
-
+                // map action settings
 
                 map.panTo(WeezeAirportLoc);
                 map.setZoom(7);
@@ -212,14 +198,13 @@ function setMapVisibility(itemClicked) {
                         position: WWUloc,
                         map: map,
                         icon: imageVenue,
-                        title: ujiTooltip,
+                        title: VenueTooltip,
                         animation: google.maps.Animation.BOUNCE
                     });
 
                 university.addListener('click', function() {
                     clearInforWindows()
                     infoVenue.open(map, university);
-                    //infoDomplatz.close();
                 });
 
                 map.panTo(WWUloc);
@@ -234,17 +219,15 @@ function setMapVisibility(itemClicked) {
                     scaledSize: new google.maps.Size(32, 32),
                 };
 
-                var contentDinnerLoc = 
-                '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
+                infoDinnerLoc = new google.maps.InfoWindow({
+                    content: '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
                     '<h2 style="color:#4686A0">Dinner Venue <br> Aposto</h2><br>' +
                     '<p align="center" style="color:#4a87d3">' +
                     '<img src="https://media.aposto.eu/aposto2017_muenster/Galerien/Impressionen-380/007_131030_APMS_S0001_S.jpg" width=50% height=50%></img>' +
                     '</br><b style="color:#4a87d3">Telephone:</b> +49 2203 404001' +
                     '</br><b style="color:#4a87d3">Address: </b> Alter Steinweg 21, 48143 Münster, Germany' +
                     '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://g.page/aposto-muenster?share">Navigate</a></h3>' +
-                    '</div>';
-                infoDinnerLoc = new google.maps.InfoWindow({
-                    content: contentDinnerLoc
+                    '</div>'
                 });
 
                 Dinner = new google.maps.Marker({
@@ -270,14 +253,13 @@ function setMapVisibility(itemClicked) {
                     scaledSize: new google.maps.Size(32, 32),
                 };
 
-                var contentMeetUp = '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
-                '<h2 style="color:#4686A0">Domplatz</h2>' +
-                '<p align="center" style="color:#4a87d3">' +
-                '<b style="color:#4a87d3">Address: Domplatz, 48149 Münster, Germany</br>' +
-                '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://goo.gl/maps/CdXqDMUsyXgWjH8Q9">Navigate</a></h3>' +
-                '</div>';
                 infoMeetUp = new google.maps.InfoWindow({
-                    content: contentMeetUp
+                    content: '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
+                    '<h2 style="color:#4686A0">Domplatz</h2>' +
+                    '<p align="center" style="color:#4a87d3">' +
+                    '<b style="color:#4a87d3">Address: Domplatz, 48149 Münster, Germany</br>' +
+                    '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://goo.gl/maps/CdXqDMUsyXgWjH8Q9">Navigate</a></h3>' +
+                    '</div>'
                 });
 
                 MeetUp = new google.maps.Marker({
@@ -295,7 +277,7 @@ function setMapVisibility(itemClicked) {
                 map.setZoom(13);
             }
             break;
-        case 'Hotel':
+        case 'hotel':
             {
 
                 //Hotels
@@ -304,22 +286,16 @@ function setMapVisibility(itemClicked) {
                 };
 
                 var Schlosspark = new google.maps.LatLng(51.9680211, 7.6082153);
-
-
-
-                var contentSchlosspark = '<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
-                '<h2 style="color:#4686A0">Hotel Am Schlosspark</h2>' +
-                '<p align="center" style="color:#4a87d3">' +
-                '<b style="color:#4a87d3">Address:</b>Schmale Str. 2-4, 48149 Münster, Germany</br>' +
-                '<b style="color:#4a87d3">Telephone:</b>+49 251 8998200</br>' +
-                '<b style="color:#4a87d3">Web Page: </b><a target="_blank" href="https://hotel-am-schlosspark-muenster.de">' +
-                'hotel-am-schlosspark-muenster.de</a></p>' +
-                '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://goo.gl/maps/SoTSTdY9UPvdoNSq5">Navigate</a></h3>' +
-                '</div>';
-
-
                 var infoSchlosspark = new google.maps.InfoWindow({
-                    content: contentSchlosspark
+                    content:'<div id="content" style="color:#4a87d3; line-height:2; padding:1%">' +
+                    '<h2 style="color:#4686A0">Hotel Am Schlosspark</h2>' +
+                    '<p align="center" style="color:#4a87d3">' +
+                    '<b style="color:#4a87d3">Address:</b>Schmale Str. 2-4, 48149 Münster, Germany</br>' +
+                    '<b style="color:#4a87d3">Telephone:</b>+49 251 8998200</br>' +
+                    '<b style="color:#4a87d3">Web Page: </b><a target="_blank" href="https://hotel-am-schlosspark-muenster.de">' +
+                    'hotel-am-schlosspark-muenster.de</a></p>' +
+                    '<h3 style="color:#4a87d3"><a class="button map"  target="_blank" href="https://goo.gl/maps/SoTSTdY9UPvdoNSq5">Navigate</a></h3>' +
+                    '</div>'
                 });
 
                 hotelSchlosspark = new google.maps.Marker({
@@ -331,7 +307,7 @@ function setMapVisibility(itemClicked) {
 
         
                 hotelSchlosspark.addListener('click', function() {
-                    clearInforWindows()
+                    clearInforWindows();
                     infoSchlosspark.open(map, hotelSchlosspark);
                 });
 
@@ -368,7 +344,7 @@ function initMap() {
         map: map,
         icon: imageVenue,
         animation: google.maps.Animation.BOUNCE,
-        title: ujiTooltip,
+        title: VenueTooltip,
     });
 
     university.addListener('click', function() {
